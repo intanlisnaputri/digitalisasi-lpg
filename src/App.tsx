0981e343f64f3ec96d69c433938f2abaaefa84e4
Mamzky <auliya77@gmail.com>
2025-10-05 12:49:39 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-2">Lembar Pemeliharaan Gudang</h1>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfS1r9yjxFi1VT92pPO605jMgQ1Z4C2PnBSbNl-oBWplFt5rg/viewform?usp=header"
+        width="100%"
+        height="600"
+        frameBorder="0"
+        marginHeight={0}
+        marginWidth={0}
+        title="Google Form"
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
+}
+
+export default App;
